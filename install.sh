a8ba0e6bef503535904d4e824fcf1d99f9def357
vam103 <15666884+vam103@users.noreply.github.com>
2021-04-07 16:36:42 +1000

    Updating to new packages
    Updating packages to new names and remove need for vagrant in conversion to WSL Ubuntu

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
-##vagrant init hashicorp/precise32
+sudo apt-get install -y apache2
 sudo apt-get update
-sudo apt-get install -y python-software-properties
+sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:ubuntugis/ppa
 sudo apt-get update
 sudo apt-get install -y python-dev
 
-sudo apt-get install -y libgdal1-dev
+## sudo apt-get install -y libgdal1-dev
 sudo apt-get install -y libgdal-dev
 sudo apt-get install -y g++
-sudo apt-get install -y python-gdal
-sudo apt-get install -y python-pip
+sudo apt-get install -y gdal-bin
+sudo apt-get install -y python3-pip
 
 sudo apt-get update
 
-sudo pip install shapely
-sudo pip install six
-sudo pip install cligj
-sudo pip install argparse
-sudo pip install ordereddict
-sudo pip install fiona
-sudo pip install Django==1.8
+sudo pip3 install shapely
+sudo pip3 install six
+sudo pip3 install cligj
+sudo pip3 install argparse
+sudo pip3 install ordereddict
+sudo pip3 install fiona
+sudo pip3 install Django==1.8
